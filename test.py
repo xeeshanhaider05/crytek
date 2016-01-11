a3cff00e3934482d38adadc938fe87ef2ea2739c
Zeeshan <xeeshanhaider05@gmail.com>
2016-01-11 12:30:41 -0800

    first commit

---
+++ test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import calendar
+import os
+
+print ("Hello World")
+inp = raw_input("Press enter to exit")
+var = 100
+
+if(var == 100):
+   print "Cool", inp
+
+calen = calendar.month(2015,12)
+print "The month"
+print calen
+
+Money = 2000
+def AddMoney():
+   # Uncomment the following line to fix the code:
+   global Money
+   Money = Money + 1
+
+print Money
+AddMoney()
+print Money
+
+print os.getcwd()
